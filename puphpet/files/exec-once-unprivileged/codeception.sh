ed61a0999dee3ec54c5963b320bb1f290c40d157
Ben Rowe <ben.rowe.83@gmail.com>
2015-05-29 23:47:16 +1000

    [TASK] initial commit of template structure

---
+++ puphpet/files/exec-once-unprivileged/codeception.sh
@@ -0,0 +1,4 @@
+composer global require "codeception/codeception=2.0.*"
+composer global require "codeception/specify=*"
+composer global require "codeception/verify=*"
+PATH=$PATH:/home/vagrant/.composer/vendor/bin

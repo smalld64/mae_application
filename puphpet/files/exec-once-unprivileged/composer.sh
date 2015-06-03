20fa9bfa225b332e16fa7d81a6688ac954df32d0
Ben Rowe <ben.rowe.83@gmail.com>
2015-06-03 20:38:17 +1000

    [TASK] updated vagrant/puppet config

---
+++ puphpet/files/exec-once-unprivileged/composer.sh
@@ -0,0 +1,5 @@
+composer global require "codeception/codeception=2.0.*"
+composer global require "codeception/specify=*"
+composer global require "codeception/verify=*"
+composer global require "fxp/composer-asset-plugin:1.0.0"
+export PATH=$PATH:/home/vagrant/.composer/vendor/bin

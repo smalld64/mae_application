5af7f2c42346eac29f157fe9ff7a53eefc90e576
Ben Rowe <ben.rowe.83@gmail.com>
2015-06-03 20:42:59 +1000

    [TASK] remove duplicate codeception script

--- puphpet/files/exec-once-unprivileged/codeception.sh
+++
@@ -1,4 +0,0 @@
-composer global require "codeception/codeception=2.0.*"
-composer global require "codeception/specify=*"
-composer global require "codeception/verify=*"
-PATH=$PATH:/home/vagrant/.composer/vendor/bin

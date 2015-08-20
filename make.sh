f9e70d41c8ef053e77009c7b6aff27d79e1515b3
Ben Rowe <ben.rowe.83@gmail.com>
2015-08-20 21:18:17 +1000

    [TASK] Recommit for permission problems

--- make.sh
+++ make.sh
@@ -1,1 +1,5 @@
-composer create-project --stability=dev --repository-url=http://toran.service.ausbum.com/repo/private/packages.json aussiebum/abs_template_yii2 app
+APP=${1:-"yii2"}
+ENV=${2:-"Development"}
+composer create-project --prefer-dist aussiebum/abs_template_$APP --repository-url=http://toran.service.ausbum.com/repo/private/ app
+
+(cd app/; ./init --env=$ENV)
